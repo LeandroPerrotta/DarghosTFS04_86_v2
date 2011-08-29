98f58f47a30a6bc2758a66129b26b8ba755f8084
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-08-29 16:52:13 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2362 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/login.lua
+++ creaturescripts/scripts/game/login.lua
@@ -100,17 +100,17 @@ function onLoginNotify(cid)
 		if(isInArray({EVENT_STATE_NONE, EVENT_STATE_INIT}, eventState)) then
 		
 			msg = (eventState == EVENT_STATE_INIT) and "Evento do dia (ABERTO!!):\n\n" or "Evento do dia:\n\n"			
-			msg = msg .. "NÃ£o se esqueÃ§a que hoje Ã© dia do evento semanal Warmaster a partir das 15:00 PM! \n\n"
-			msg = msg .. "O Warmaster Ã© um evento de PvP que acontece as terÃ§as e domingos e premia o vencedor com um ticket para o Warmaster Outfit. \n"
-			msg = msg .. "Para entrar no evento va para o deserto ao oeste de Quendor e lÃ¡ vocÃª encontrarÃ¡ uma estrutura com teleports que levam para dentro do evento.\n"
-			msg = msg .. "Dentro do evento tudo Ã© Hardcore PvP e se vocÃª morrer vocÃª nÃ£o perderÃ¡ nada. O objetivo Ã© simplesmente destruir os obstaculos e se manter vivo!\n"
-			msg = msg .. "Na ultima sala existirÃ¡ o boss que ao ser derrotado droparÃ¡ o prÃªmio!\n"
+			msg = msg .. "Não se esqueça que hoje é dia do evento semanal Warmaster a partir das 15:00 PM! \n\n"
+			msg = msg .. "O Warmaster é um evento de PvP que acontece as terças e domingos e premia o vencedor com um ticket para o Warmaster Outfit. \n"
+			msg = msg .. "Para entrar no evento va para o deserto ao oeste de Quendor e lá você encontrará uma estrutura com teleports que levam para dentro do evento.\n"
+			msg = msg .. "Dentro do evento tudo é Hardcore PvP e se você morrer você não perderá nada. O objetivo é simplesmente destruir os obstaculos e se manter vivo!\n"
+			msg = msg .. "Na ultima sala existirá o boss que ao ser derrotado dropará o premio!\n"
 		end
 	elseif(today == WEEKDAY.MONDAY and getPlayerLevel(cid) >= 80) then
 	
 		msg = "Lembrete do dia:\n\n"
-		msg = msg .. "Hoje Ã© segunda-feira e o barco que faz viagens Quendor (PvP) <-> Island of Peace (Optional PvP) estÃ¡ disponivel caso vocÃª deseje transferir seu personagem! \n"
-		msg = msg .. "Pense bem e lembre-se que sÃ³ Ã© permitida UMA unica viagem e que caso seja feita vocÃª terÃ¡ de permanecer no destino escolhido ao menos atÃ© a proxima segunda-feira!\n"
+		msg = msg .. "Hoje é segunda-feira e o barco que faz viagens Quendor (PvP) <-> Island of Peace (Optional PvP) está disponivel caso você deseje transferir seu personagem! \n"
+		msg = msg .. "Pense bem e lembre-se que só é permitida UMA unica viagem e que caso seja feita você terá de permanecer no destino escolhido ao menos até a proxima segunda-feira!\n"
 	end
 	
 	if(msg ~= nil) then
