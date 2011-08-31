3e92b620ccdca799ea51c07b289ab15f0ed43e7e
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-08-31 21:01:51 +0000

    * onLogin Item Shop
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2398 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ lib/darghos/lib_item_shop.lua
@@ -0,0 +1,154 @@
+--- Private vars
+itemShop = {
+	count = 0,
+	receive_list = nil,
+	not_received_list = nil,
+	cid = nil
+}
+
+function itemShop:new()
+
+	local obj = {}
+	obj.cid = nil
+	obj.receive_list = {}
+	obj.not_received_list = {}
+	
+	setmetatable(obj, self)
+	self.__index = self
+	return obj
+end
+
+function itemShop:log(msg, log_id)
+
+	local log_str = (log_id ~= nil) and "| LogID " .. log_id .. " " or ""
+	
+	local out = os.date("%X") .. " | Player [" .. getPlayerName(self.cid) .. "] " .. log_str .. "| " .. msg .. "."
+	
+	local date = os.date("*t")
+	local fileStr = date.day .. "-" .. date.month .. ".log"
+	local patch = getDataDir() .. "logs/itemshop/"
+	local file = io.open(patch .. fileStr, "a+")
+	
+	file:write(out .. "\n")
+	file:close()
+end
+
+function itemShop:markReceived(log_id)
+	db.executeQuery("UPDATE `wb_itemshop_log` SET `received` = 1 WHERE `id` = " .. log_id .. ";")
+end
+
+function itemShop:notify()
+
+	local msg = ""
+	
+	if(self.count == #self.receive_list and #self.not_received_list == 0) then
+		msg = msg + "Todas as suas " .. #self.receive_list .. " compras obtidas no Item Shop foram entregues com sucesso em sua backpack principal!\n\n"
+		msg = msg + "Tenha um bom jogo!"		
+	elseif(#self.not_received_list > 0 and #self.not_received_list < #self.receive_list) then
+		local receivedItems = #self.receive_list - #self.not_received_list
+		msg = msg + "Parte de suas compras (" .. receivedItems .. ") obtidas no Item Shop foram entregues com sucesso em sua backpack principal!\n\n"
+		msg = msg + "Obs:\n"
+		msg = msg + "Ainda há " .. #self.not_received_list .." compra(s) pendentes para serem entregue(s), por favor, certifique-se de possuir capacidade para carregar e slots livres em sua backpack principal e re-faça o login para receber as compras. Obrigado."
+	elseif(#self.not_received_list == #self.receive_list) then
+		msg = msg + "Há " .. #self.receive_list .. "  compra(s) no Item Shop pendentes para serem entregue(s), por favor, certifique-se de possuir capacidade para carregar e slots livres em sua backpack principal e re-faça o login para receber a(s) compra(s). Obrigado."	
+	else
+		self:log("Problema ilogico não previsto: Total de itens " .. self.count .. ", Itens não recebidos " .. #self.not_received_list .. ", Itens a Receber " .. #self.receive_list .. ".")
+		return
+	end
+
+	doPlayerSendTextMessage(self.cid, MESSAGE_STATUS_CONSOLE_ORANGE, msg)
+end
+
+function itemShop:giveList()
+
+	self.not_received_list = self.receive_list
+
+	for _, item in pairs(self.receive_list) do
+		if(self:giveItem(item)) then
+			self:markReceived(item.log_id)
+			self.not_received_list[_] = nil
+		else
+			self:log("Impossivel adicionar mais itens, processo abortado. Itens restantes : " .. json.encode(self.not_received_list) .. "")
+			break
+		end
+	end
+end
+
+function doItemSetItemShopLogId(uid, log_id)
+	return doItemSetAttribute(uid, "itemShopLogId", log_id)
+end
+
+function itemShop:giveItem(data)
+
+	local PRESENT_BOX = 1990
+	
+	local container = doCreateItemEx(PRESENT_BOX, 1)
+	local item = doCreateItemEx(data.item_id, data.item_count)	
+
+	if(data.item_action_id ~= nil) then
+		doItemSetActionId(item, data.item_action_id)
+	end
+	
+	doItemSetItemShopLogId(item, data.log_id)
+	
+	if(doAddContainerItemEx(container, item) == LUA_ERROR) then
+		self:log("Impossivel colocar em container " .. json.encode(data) .. "")
+		return false
+	end
+	
+	if(doPlayerAddItemEx(self.cid, container, false) ~= RETURNVALUE_NOERROR) then
+		self:log("Item Data Entregue com sucesso: " .. json.encode(data) .. "")
+		return true
+	end	
+	
+	self:log("Impossivel colocar no inventario " .. json.encode(data) .. "")
+	return false
+end
+
+-- Verifica se o player possui um item no shop a receber
+-- Usado em creaturescripts/login.lua
+function itemShop:onLogin(cid)
+
+	local result = db.getResult("SELECT `shop`.`name`, `shop`.`params`, `log`.`id` FROM `wb_itemshop_log` `log` LEFT JOIN `wb_itemshop` ON `log`.`shop_id` = `shop`.`id` WHERE `log`.`player_id` = " .. getPlayerGUID(cid) .. " AND `log`.`received` = 0;")
+	
+	local totalWeight = 0
+	
+	if(result:getID() ~= -1) then
+	
+		self.cid = cid
+		self.count = result:getRows(false)
+		self:log("Processando " .. self.count .. " itens a entregar...")
+			
+		repeat
+			local json = require("json")
+			local item_params = json.decode(result:getDataString("params"))
+			local item_action_id = item_params["item_action_id"] or 0
+			
+			totalWeight = totalWeight + getItemWeightById(item_params["item_id"], item_params["item_count"])
+			
+			if(getPlayerFreeCap(cid) > totalWeight) then
+						
+				table.insert(self.receive_list, {
+					log_id = result:getDataInt("id"), 
+					item_id = item_params["item_id"], 
+					item_count = item_params["item_count"], 
+					item_name = result:getDataString("name"), 
+					item_action_id = item_action_id
+				})
+				
+				self:log("Item Data: " .. json.encode(self.receive_list[#self.receive_list]) .. "")
+			else
+				self:log("Capacidade esgotada, entregando os itens previamente processados.")
+				break
+			end
+					
+		until not(result:next())
+		result:free()
+		
+		self:giveList()
+		self:notify()
+		
+		self:log("Processo concluido.")
+		self:log("--------------------------------------")
+	end
+end
