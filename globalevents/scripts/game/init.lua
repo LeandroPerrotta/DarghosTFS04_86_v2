5f7068878ae761075586f8af004b793283b77cff
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-01 05:33:59 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2416 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- globalevents/scripts/game/init.lua
+++ globalevents/scripts/game/init.lua
@@ -29,21 +29,6 @@ function onStartup()
 	addEvent(autoBroadcast, 1000 * 60 * 30)
 	
 	luaGlobal.truncate()
-	
-	local g_table = global_table("testTable")
-	
-	if(g_table["somevalue"] == nil) then
-		g_table["somevalue"] = "myvalue"
-	end
-	
-	g_table[3] = 45
-	print(table.show(g_table))
-	
-	table.insert(g_table, "inserted value")
-	
-	print(table.show(g_table))
-	
-	
 	return true
 end
 
