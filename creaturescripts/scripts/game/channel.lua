a69ee5ddfa81a0ea378d83e47e7c718589cf5aae
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-02 21:32:09 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2434 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/channel.lua
+++ creaturescripts/scripts/game/channel.lua
@@ -2,9 +2,11 @@ function onLeaveChannel(cid, channel, users)
 
 	if(isPlayer(cid) and channel == CUSTOM_CHANNEL_PVP) then
 		doPlayerSendCancel(cid, "Não é permitido fechar este canal.")
-		addEvent(doPlayerOpenChannel, 150, cid, CUSTOM_CHANNEL_PVP)
+		addEvent(reopenChannel, 150, cid, CUSTOM_CHANNEL_PVP)
 		return true
 	end
 	
 	return true
 end
+
+function reopenChannel(cid, channel) return ((isPlayer(cid)) and doPlayerOpenChannel(cid, CUSTOM_CHANNEL_PVP) or nil) end
