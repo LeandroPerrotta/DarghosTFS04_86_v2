0a1eafe88600c59c9b56f115d19ca980581bd8d4
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-07 17:16:43 +0000

    * Acho que tá pronto para ir online :S
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2512 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/salazart.joia.lua
+++ npc/scripts/game/salazart.joia.lua
@@ -14,9 +14,10 @@ local ITEM_KOSHEI_AMULET = 8266
 function hasAllPieces(cid, pieces)
 	local found = 0
 	for k,v in pairs(pieces) do
-		if(getPlayerItemCount(cid, v) >= 1) then found = found + 1 end
-		return (found == #pieces) and true or false
+		if(getPlayerItemCount(cid, v) >= 1) then found = found + 1 end		
 	end
+	
+	return found >= #pieces
 end
 
 function takeAllPieces(cid, pieces)
@@ -34,20 +35,19 @@ function process(cid, message, keywords, parameters, node)
 	
     if(not npcHandler:isFocused(cid)) then
         return false
-    end
-    
+    end 
     
     local alreadyHasAmulet = (getPlayerStorageValue(cid, sid.GET_KOSHEI_AMULET) == 1) and true or false
     local amuletDate = getPlayerStorageValue(cid, sid.KOSHEI_AMULET_DATE)
     local hasAllPieces = hasAllPieces(cid, AMULET_PIECES)
     
-    if(talk_state == 1) then
+    if(talkState == 1) then
     	if(not alreadyHasAmulet) then
-    		if(hasAllPieces) then
+    		if(hasAllPieces or amuletDate ~= 1) then
     			if(amuletDate == -1) then
-    				npcHandler:say("Oooohhh! Quanto tempo eu n√£o via isto? Todas as partes reunidas! Magnifico trabalho! Se voc√™ me entregar-las e mais 5000 moedas de ouro para amanha eu lhe entregarei o amuleto do velho Koshei, aceita?")
+    				npcHandler:say("Oooohhh! Quanto tempo eu n„o via isto? Todas as partes reunidas! Magnifico trabalho! Se vocÍ me entregar-las e mais 5000 moedas de ouro para amanha eu lhe entregarei o amuleto do velho Koshei, aceita?", cid)
     			elseif(amuletDate + (60 * 60 * 24) > os.time()) then
-    				npcHandler:say("Mas ainda n√£o se passou um dia, o seu amuleto ainda n√£o est√° pronto! Deixe-me trabalhar ou ir√° atrazar!")
+    				npcHandler:say("Mas ainda n„o se passou um dia, o seu amuleto ainda n„o est· pronto! Deixe-me trabalhar ou ir· atrazar!", cid)
     				npcHandler:resetNpc()
 				else
 					local tmp = doCreateItemEx(ITEM_KOSHEI_AMULET, 1)
@@ -59,31 +59,31 @@ function process(cid, message, keywords, parameters, node)
 					
 					setPlayerStorageValue(cid, sid.GET_KOSHEI_AMULET, 1)
 							
-					npcHandler:say("Que bom lhe ver, por que tanta demora?? Tenho boas noticias! Seu amuleto est√° pronto! Aqui est√°! Espero que fa√ßa bom aproveito!")
+					npcHandler:say("Que bom lhe ver, por que tanta demora?? Tenho boas noticias! Seu amuleto est· pronto! Aqui est·! Espero que faÁa bom aproveito!", cid)
     			end
     		else
-    			npcHandler:say("Ent√£o quer saber do amuleto? Ohh.. Ok! Estamos falando de um artefato muito poderoso, o amuleto de Koshei, uma criatura que acreditam ser imortal! E eu posso o fazer, voc√™ o gostaria?")
+    			npcHandler:say("Ent„o quer saber do amuleto? Ohh.. Ok! Estamos falando de um artefato muito poderoso, o amuleto de Koshei, uma criatura que acreditam ser imortal! E eu posso o fazer, vocÍ o gostaria?", cid)
     		end		
     	else
-    		npcHandler:say("Mas que pena, n√£o conhe√ßo outros amuletos especiais al√©m do que j√° lhe fiz...")
+    		npcHandler:say("Mas que pena, n„o conheÁo outros amuletos especiais alÈm do que j· lhe fiz...", cid)
     		npcHandler:resetNpc()
     	end
-    elseif(talk_state == 2) then
+    elseif(talkState == 2) then
     	if(hasAllPieces) then
     		if(takeAllPieces(cid, AMULET_PIECES)) then
     			doPlayerRemoveMoney(cid, 5000)
-    			playerSetStorageValue(cid, sid.KOSHEI_AMULET_DATE, os.time())
-    			npcHandler:say("Otimo, agora m√™ de um dia para trabalhar na restaura√ß√£o, retorne amanha e lhe entregarei o amuleto!")
+    			setPlayerStorageValue(cid, sid.KOSHEI_AMULET_DATE, os.time())
+    			npcHandler:say("Otimo, agora mÍ de um dia para trabalhar na restauraÁ„o, retorne amanha e lhe entregarei o amuleto!", cid)
 			else
 				npcHandler:resetNpc()
 				return false
     		end
     	else
-    		npcHandler:say("Bom, para conseguir-lo n√£o ser√° facil, pois o amuleto foi partido em quatro partes que est√£o espalhadas por todo deserto de Salazart. Mas eu sei o lugar aproximado em que elas est√£o, voc√™ precisar√° buscar-las, certo?")
+    		npcHandler:say("Bom, para conseguir-lo n„o ser· facil, pois o amuleto foi partido em quatro partes que est„o espalhadas por todo deserto de Salazart. Mas eu sei o lugar aproximado em que elas est„o, vocÍ precisar· buscar-las, certo?", cid)
     	end 	
-    elseif(talk_state == 3) then
-    	npcHandler:say("Preste aten√ß√£o: O paradeiro da primeira parte √© uma camara no sub-solo n√£o muito longe daqui ao sudoeste, proximo a entrada para o barco da cidade, procure-o nas raizes do ch√£o. Para a segunda parte siga ao sudeste, procure na proa do barco dos piratas esqueletos.")
-    	npcHandler:say("Para a terceira parte siga ao norte, e voc√™ encontrar√° um pequeno o√°sis, procure nas arvores. E finalmente para a quarta e √∫ltima parte siga ao noroeste pela margem do oceano, logo quando este der lugar a montanha examine as rochas. Retorne quando estiver com todas as partes.")
+    elseif(talkState == 3) then
+    	npcHandler:say("Preste atenÁ„o: O paradeiro da primeira parte È uma camara no sub-solo n„o muito longe daqui ao sudoeste, proximo a entrada para o barco da cidade, procure-o nas raizes do ch„o. Para a segunda parte siga ao sudeste, procure na proa do barco dos piratas esqueletos.", cid)
+    	npcHandler:say("Para a terceira parte siga ao norte, e vocÍ encontrar· um pequeno o·sis, procure nas arvores. E finalmente para a quarta e ˙ltima parte siga ao noroeste pela margem do oceano, logo quando este der lugar a montanha examine as rochas. Retorne quando estiver com todas as partes.", cid)
     end
     
     return true
@@ -92,9 +92,9 @@ end
 local node1 = keywordHandler:addKeyword({'amuleto'}, process, {npcHandler = npcHandler, onlyFocus = true, talk_state = 1})
     local node2 = node1:addChildKeyword({'yes', 'sim'}, process, {npcHandler = npcHandler, onlyFocus = true, talk_state = 2})
    		node2:addChildKeyword({'yes', 'sim'}, process, {npcHandler = npcHandler, onlyFocus = true, talk_state = 3})   
-    	node2:addChildKeyword({'no', 'n√£o', 'n√£o'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Que pena... Tem algo mais em que eu possa lhe ajudar?', reset = true})
+    	node2:addChildKeyword({'no', 'n„o', 'n„o'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Que pena... Tem algo mais em que eu possa lhe ajudar?', reset = true})
     
-    node1:addChildKeyword({'no', 'n√£o', 'n√£o'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Que pena... Tem algo mais em que eu possa lhe ajudar?', reset = true})
+    node1:addChildKeyword({'no', 'n„o', 'n„o'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Que pena... Tem algo mais em que eu possa lhe ajudar?', reset = true})
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
