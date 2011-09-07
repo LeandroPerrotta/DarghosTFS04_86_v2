1fbb1706bc6dae6e1da3ce149d3c268ddddd58ac
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-07 17:16:43 +0000

    * Acho que tá pronto para ir online :S
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2512 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/death.lua
+++ creaturescripts/scripts/game/death.lua
@@ -1,6 +1,6 @@
 function onDeath(cid, corpse, deathList)
 	if isPlayer(cid) == TRUE then
-		--Funï¿½ï¿½es que serï¿½o chamadas quando um jogador morrer...
+		--Fun??es que ser?o chamadas quando um jogador morrer...
 		
 		Dungeons.onPlayerDeath(cid)
 		setPlayerStorageValue(cid, sid.GIVE_ITEMS_AFTER_DEATH, 1)
@@ -13,20 +13,18 @@ function onDeath(cid, corpse, deathList)
 			function resummonKoshei(cid, corpse)
 			
 				if(getGlobalStorageValue(gid.KOSHEI_DEATH) == 1) then
-					doCreatureSay(cid, "Aaarrghhh!! Este miseravel conhece minha fraquesa!! Eu ainda retornarei!!", TALKTYPE_ORANGE_1)
 					setGlobalStorageValue(gid.KOSHEI_DEATH, -1)
 					return
 				end
 			
-				doCreatureSay(cid, "Mas que tolice! Eu sou IMORTAL MUAHAHAHAHHA", TALKTYPE_ORANGE_1)
-				doRemoveItem(corpse.uid)
 				local pos = getThingPos(uid.KOSHEI_POS)
-				local koshei = doSummonCreature("Koshei the Deathless")
-				addCreatureEvent(koshei, "monsterDeath")				
+				local koshei = doSummonCreature("Koshei the Deathless", pos)
+				registerCreatureEvent(koshei, "monsterDeath")	
+				doCreatureSay(koshei, "Mas que tolice! Eu sou IMORTAL MUAHAHAHAHHA", TALKTYPE_ORANGE_1)				
 			end
 		
-			doCreatureSay(cid, "Argh! VocÃª realmente acha que me derrotou? <...>", TALKTYPE_ORANGE_1)
-			doItemSetAttribute(corpse.uid, "kosheiCorpse", true)
+			doCreatureSay(cid, "Argh! Você realmente acha que me derrotou? <...>", TALKTYPE_ORANGE_1)
+			doItemSetAttribute(corpse.uid, "kosheiDeathDate", os.time())
 			addEvent(resummonKoshei, 1000 * 4, cid, corpse)
 		end
 	end
