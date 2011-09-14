f76f9ed8ea286ca5b046b396847095cf6c7bade2
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-14 13:01:19 +0000

    * fixies...
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2574 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/bless/pvpbless.lua
+++ npc/scripts/game/bless/pvpbless.lua
@@ -8,9 +8,9 @@ function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid) 		end
 function onCreatureSay(cid, type, msg) 		npcHandler:onCreatureSay(cid, type, msg) 	end
 function onThink() 							npcHandler:onThink() 						end
 
-local node1 = keywordHandler:addKeyword({'twist of fate', 'pvp bless'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Saiba que a benÃ§Ã£o do PvP (twist of fate) nÃ£o irÃ¡ reduzir a penalidade quando vocÃª morre como as benÃ§Ãµes normais, mas ao invez disto, irÃ¡ previnir que vocÃª perca as proprias benÃ§Ãµes normais quando vocÃª for derrotado por outro jogador (apÃ©nas jogadores!). VocÃª quer receber esta proteÃ§Ã£o pelo sacrificio de 2000 moedas de ouro mais 100 moedas adicionais por cada level entre o 30 e 120?'})
+local node1 = keywordHandler:addKeyword({'twist of fate', 'pvp bless'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Saiba que a benção do PvP (twist of fate) não irá reduzir a penalidade quando você morre como as benções normais, mas ao invez disto, irá previnir que você perca as proprias benções normais quando você for derrotado por outro jogador (apénas jogadores!). Você quer receber esta proteção pelo sacrificio de 2000 moedas de ouro mais 100 moedas adicionais por cada level entre o 30 e 120?'})
 	node1:addChildKeyword({'yes', 'sim'}, D_CustomNpcModules.pvpBless, {npcHandler = npcHandler, baseCost = 2000, levelCost = 100, startLevel = 30, endLevel = 120})
-	node1:addChildKeyword({'no', 'nÃ£o', 'nao'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'Posso lhe ajudar em algo mais?'})
+	node1:addChildKeyword({'no', 'não', 'nao'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'Posso lhe ajudar em algo mais?'})
 
 --local node1 = keywordHandler:addKeyword({'twist of fate'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Sorry, but the bless twist of fate are unavailable for now. Wait some time and back again.'})
 
