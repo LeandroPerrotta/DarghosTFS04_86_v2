e994ccb33d6ec9674a974fa349461b9d745b6101
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-14 12:56:56 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2573 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- lib/darghos/functions.lua
+++ lib/darghos/functions.lua
@@ -1,6 +1,6 @@
 function getPlayerPVPBlessing(cid)
 	local pvpBless = getConfigValue('useBlessingAsPvp')
-	return getPlayerBlessings(cid, pvpBless) or false
+	return getPlayerBlessing(cid, pvpBless) or false
 end
 
 function setPlayerPVPBlessing(cid)
