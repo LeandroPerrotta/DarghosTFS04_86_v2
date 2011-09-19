9ca88adb6ad53acfcb1e509e71d95d52da50ef64
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-19 22:05:40 +0000

    * otimizado funcionamento das blessings
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2605 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/bless/bless2.lua
+++ npc/scripts/game/bless/bless2.lua
@@ -8,8 +8,6 @@ function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid) 		end
 function onCreatureSay(cid, type, msg) 		npcHandler:onCreatureSay(cid, type, msg) 	end
 function onThink() 							npcHandler:onThink() 						end
 
-local node1 = keywordHandler:addKeyword({'bless'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Do you want to buy one part of blessing for 2000 and extra 200 gold for each level between 30 and 120?'})
-	node1:addChildKeyword({'yes'}, StdModule.bless, {npcHandler = npcHandler, number = 2, premium = false, baseCost = 2000, levelCost = 200, startLevel = 30, endLevel = 120})
-	node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'Too expensive, eh?'})
+keywordHandler:addKeyword({'bless', 'benção', 'bencao'}, D_CustomNpcModules.offerBlessing, {npcHandler = npcHandler, onlyFocus = true, number = 2, premium = false, baseCost = 2000, levelCost = 200, startLevel = 30, endLevel = 120})
 
 npcHandler:addModule(FocusModule:new())
--- npc/scripts/game/bless/pvpbless.lua
+++ npc/scripts/game/bless/pvpbless.lua
@@ -8,9 +8,7 @@ function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid) 		end
 function onCreatureSay(cid, type, msg) 		npcHandler:onCreatureSay(cid, type, msg) 	end
 function onThink() 							npcHandler:onThink() 						end
 
-local node1 = keywordHandler:addKeyword({'twist of fate', 'pvp bless'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Saiba que a benção do PvP (twist of fate) não irá reduzir a penalidade quando você morre como as benções normais, mas ao invez disto, irá previnir que você perca as proprias benções normais quando você for derrotado por outro jogador (apénas jogadores!). Você quer receber esta proteção pelo sacrificio de 2000 moedas de ouro mais 100 moedas adicionais por cada level entre o 30 e 120?'})
-	node1:addChildKeyword({'yes', 'sim'}, D_CustomNpcModules.pvpBless, {npcHandler = npcHandler, baseCost = 2000, levelCost = 100, startLevel = 30, endLevel = 120})
-	node1:addChildKeyword({'no', 'não', 'nao'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'Posso lhe ajudar em algo mais?'})
+keywordHandler:addKeyword({'bless', 'benção', 'bencao'}, D_CustomNpcModules.offerBlessing, {npcHandler = npcHandler, onlyFocus = true, ispvp = true, baseCost = 2000, levelCost = 200, startLevel = 30, endLevel = 270})
 
 --local node1 = keywordHandler:addKeyword({'twist of fate'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Sorry, but the bless twist of fate are unavailable for now. Wait some time and back again.'})
 
