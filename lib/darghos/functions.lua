02a0a80e6ce0a1437d2ac8228849934e7cbb8f42
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-09-21 19:38:13 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2609 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- lib/darghos/functions.lua
+++ lib/darghos/functions.lua
@@ -1267,7 +1267,6 @@ function hasPollToNotify(cid)
 
 	local result = db.getResult("SELECT `id`, `text` FROM `wb_forum_polls` WHERE `end_date` > UNIX_TIMESTAMP();")
 	if(result:getID() == -1) then
-		result:free()
 		return false
 	end
 	
@@ -1281,10 +1280,11 @@ function hasPollToNotify(cid)
 	result = db.getResult("SELECT `user`.`id` FROM `wb_forum_users` `user` LEFT JOIN `wb_forum_user_votes` `vote` ON `vote`.`user_Id` = `user`.`id` LEFT JOIN `wb_forum_polls_opt` `opt` ON `opt`.`id` = `vote`.`opt_id` WHERE `user`.`account_id` = " .. account .. " AND `opt`.`poll_id` = " .. poll.id .. ";")
 
 	if(result:getID() == -1) then
-		result:free()
 		setPlayerStorageValue(cid, sid.WEBSITE_POLL_NOTIFY, 1)
 		return poll
 	end
+	
+	result:free()
 	
 	return false
 end
