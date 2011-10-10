5e2b16358d19c412226bedd8dd358c86172a0fb6
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-10-10 22:42:48 +0000

    * fix paralize * fix loot e resistencias/fraquezas Ushuriel
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2692 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- spells/scripts/support/paralyze_rune.lua
+++ spells/scripts/support/paralyze_rune.lua
@@ -1,10 +1,11 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 
 local condition = createConditionObject(CONDITION_PARALYZE)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 20000)
-setConditionFormula(condition, -0.9, 0, -0.9, 0)
+setConditionFormula(condition, -0.80, 0, -0.80, 0)
 setCombatCondition(combat, condition)
 
 function onCastSpell(cid, var)
