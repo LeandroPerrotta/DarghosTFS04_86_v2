f0cb9471c3df2f1d77e4bd0a5bef46d53f129601
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-10-23 07:18:33 +0000

    * trainers em quendor (nao há mais ilha de treinamento) * POI e Inquisition Optional PvP * Respawns Bosses Inquisition não bloqueavel (via raid)
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2760 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/boats/kashmir.lua
+++ npc/scripts/game/boats/kashmir.lua
@@ -13,11 +13,9 @@ boatDestiny.addAracura(keywordHandler, npcHandler)
 boatDestiny.addAaragon(keywordHandler, npcHandler)    
 boatDestiny.addQuendor(keywordHandler, npcHandler)    
 boatDestiny.addNorthrend(keywordHandler, npcHandler)    
-boatDestiny.addTrainers(keywordHandler, npcHandler, D_CustomNpcModules.travelTrainingIsland)
         
-keywordHandler:addKeyword({'passage'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can take you to Aracura, Quendor, Northrend and Aaragon also Trainers.'})
+keywordHandler:addKeyword({'passage', 'travel', 'passagem', 'viajar', 'cidades'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can take you to Aracura, Quendor and Northrend also Aaragon.'})
 keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I am the captain of this ship.'})
-keywordHandler:addKeyword({'travel'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can take you to Aracura, Quendor, Northrend and Aaragon also Trainers.'})
 
 -- Makes sure the npc reacts when you say hi, bye etc.
 npcHandler:addModule(FocusModule:new())
