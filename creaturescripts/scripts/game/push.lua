525572eff14304f7b74b78de9766c1cf18858e3c
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-10-24 05:30:56 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2770 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/push.lua
+++ creaturescripts/scripts/game/push.lua
@@ -3,9 +3,9 @@ function onPush(cid, target)
 	if(isPlayer(cid) and isPlayer(target)) then	
 		local tile = getTileInfo(getCreaturePosition(cid))
 		if(tile.optional) then		
-			local target = getCreatureTarget(cid)
-			if(getBooleanFromString(target) and isInArray({"Marksman Target", "Hitdoll"}, getCreatureName(target))) then
-				doPlayerSendCancel(cid, "N√£o o encomode! Ele est√° treinando concentradamente!")	
+			local attacked = getCreatureTarget(target)
+			if(getBooleanFromString(attacked) and isInArray({"Marksman Target", "Hitdoll"}, getCreatureName(attacked))) then
+				doPlayerSendCancel(cid, "N„o o encomode! Ele est· treinando concentradamente!")	
 				return false
 			end
 		end	
