a86d36e9f3622830797259dc78dcb05bc5ed3818
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-10-31 22:27:36 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2779 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- npc/scripts/game/rashid.lua
+++ npc/scripts/game/rashid.lua
@@ -18,7 +18,7 @@ function greetCallback(cid)
 		end
 
         if(pvpBattleground.getPlayerRating(cid) < 500) then
-                npcHandler:say('Eu compro uma grande variedade de armas e equipamentos por um bom preço! Mas somente negocio com bravos guerreiros! Para comprovar seu valor faça vença algumas Battlegrounds ({!bg entrar}) até atingir 500 pontos de classificação e terei prazer em negociar com você!', cid)
+                npcHandler:say('Eu compro uma grande variedade de armas e equipamentos por um bom preço! Mas somente negocio com bravos guerreiros! Para comprovar seu valor vença algumas Battlegrounds ({!bg entrar}) até atingir 500 pontos de classificação e terei prazer em negociar com você!', cid)
                 return false
         else
                 return true
