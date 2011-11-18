20e9cf577656c8183644315f9450285934707d6d
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-11-18 14:28:20 +0000

    * parte do ultimo commit
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2854 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- actions/scripts/game/annihilator.lua
+++ actions/scripts/game/annihilator.lua
@@ -1,7 +1,7 @@
 local INTERVAL_ENTER = 10 * 60 -- 10 minutes
 
-local players = {}
-local demons = {}
+players = {}
+demons = {}
 
 local function loadValues(actionid)
 
@@ -38,7 +38,7 @@ local function loadValues(actionid)
 		table.insert(demons, uid.ANIHI_ARACURA_DEMON6)		
 		table.insert(demons, uid.ANIHI_ARACURA_DEMON7)		
 	else
-		--debugPrint("Unknown actionid type.")
+		error("[Annihilator] Unknown actionid for location: " .. actionid)
 	end
 
 end
@@ -64,58 +64,61 @@ function canTeleportPlayers(cid)
 				stats = ERROR.NEED_ALL_PLAYERS
 				break
 			end
+			
+			if(getPlayerStorageValue(pid, sid.ANIHI_COMPLETE) ~= -1) then
+				stats = ERROR.PLAYER_ALREADY_MADE_QUEST
+				break
+			end
+			
+			players[k].cid = pid			
 		else
 			stats = ERROR.NEED_ALL_PLAYERS
 			break
 		end
-		
-		if(getPlayerStorageValue(pid, sid.ANIHI_COMPLETE) ~= -1) then
-			stats = ERROR.PLAYER_ALREADY_MADE_QUEST
-			break
-		end
-		
-		v["cid"] = pid
 	end
 	
 	if(stats == ERROR.NONE) then
 		-- inserimos o objeto do jogador para ser usado posteriormente
 		return true
+	end
+	
+	if(stats == ERROR.PLAYER_ALREADY_MADE_QUEST) then
+		doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "A player of this team already have done this quest.")
+	elseif(stats == ERROR.NEED_ALL_PLAYERS) then
+		doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "Its nescessary four players to own this quest.")
 	else
-		if(stats == ERROR.PLAYER_ALREADY_MADE_QUEST) then
-			doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "A player of this team already have done this quest.")
-		elseif(stats == ERROR.NEED_ALL_PLAYERS) then
-			doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "Its nescessary four players to own this quest.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "For an unknown reason can not be make this action. An log has been reported to admin.")
-			--debugPrint("Unknown error code for checking players.")
-		end
-		
-		return false
-	end	
+		doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "For an unknown reason can not be make this action. An log has been reported to admin.")
+		--debugPrint("Unknown error code for checking players.")
+	end
+	
+	return false
 end
 
 local function teleportPlayers()
 	for k,v in pairs(players) do
-		local pos = getCreaturePosition(v.cid)
-		doSendMagicEffect(pos, CONST_ME_TELEPORT)
-		doTeleportThing(v.cid, getThingPos(v.dest))
-		doSendMagicEffect(getThingPos(v.dest), CONST_ME_TELEPORT)
+		if(v.cid) then
+			local pos = getCreaturePosition(v.cid)
+			doSendMagicEffect(pos, CONST_ME_TELEPORT)
+			doTeleportThing(v.cid, getThingPos(v.dest))
+			doSendMagicEffect(getThingPos(v.dest), CONST_ME_TELEPORT)
+		end
 	end
 end
 
 local function summonDemons()
+
 	for k,v in pairs(demons) do
 		local demon_pos = getThingPos(v)
 		doSummonCreature("demon", demon_pos)
 	end
 end
 
-local function removeDemons()
+function removeDemons()
 	for k,v in pairs(demons) do
 		local demon_pos = getThingPos(v)
 		local demon = getTopCreature(demon_pos).uid
 		
-		if(demon ~= 0 and isMonster(demon) == TRUE) then
+		if(demon ~= 0 and isMonster(demon)) then
 			doRemoveCreature(demon)
 		end
 	end
@@ -126,7 +129,7 @@ local function removeDemons()
 		local pid = getTopCreature(player_pos).uid
 		
 		if(pid ~= 0 and isMonster(pid)) then
-			doRemoveCreature(pid)
+			doRemoveCreature(pid)	
 		end
 	end	
 end
@@ -135,7 +138,7 @@ function onUse(cid, item, frompos, item2, topos)
 
 	if item.itemid ~= 1945 and item.itemid ~= 1946 then
 		--debugPrint("Wrong item id?")
-		return TRUE
+		return true
 	end
 
 	local lastEnter = getGlobalStorageValue(gid.ANIHI_TIMER)
@@ -149,8 +152,8 @@ function onUse(cid, item, frompos, item2, topos)
 			
 			setGlobalStorageValue(gid.ANIHI_TIMER, os.time())
 			addEvent(anihi_timmer, INTERVAL_ENTER * 1000)
-		else
-			return TRUE
+			
+			return false
 		end		
 	else
 		local secondsLeft = ((lastEnter + INTERVAL_ENTER) - os.time()) 
@@ -161,8 +164,9 @@ function onUse(cid, item, frompos, item2, topos)
 		else
 			doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "An another team has recently actived the lever. You must wait less then a minute to activate it again.")
 		end
-		return TRUE	
 	end
+	
+	return true
 end
 
 function anihi_timmer()
