d5489365a48da4fb4a07aaa9f549e3fcd73f9429
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-12-03 06:18:53 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2898 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- lib/darghos/stages.lua
+++ lib/darghos/stages.lua
@@ -121,7 +121,7 @@ function changeStage(cid, skilltype, multiple)
 			changePvpDebuff = round(darghos_change_pvp_debuff_percent / 100, 2)
 		end
 		
-		local expSpecialBonus = 0
+		local expSpecialBonus = 1
 		local expSpecialBonusEnd = getPlayerStorageValue(cid, sid.EXP_MOD_ESPECIAL_END)
 		 if(expSpecialBonusEnd ~= -1  and os.time() <= expSpecialBonusEnd) then
 		 	expSpecialBonus = (getPlayerStorageValue(cid, sid.EXP_MOD_ESPECIAL) > 0) and round(getPlayerStorageValue(cid, sid.EXP_MOD_ESPECIAL) / 100, 2) or 0
