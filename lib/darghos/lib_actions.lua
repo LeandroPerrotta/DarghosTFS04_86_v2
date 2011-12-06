53d38b2707afd4b1cc42a4e3ed0c90a57c31a204
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-12-06 23:10:03 +0000

    * Adicionado suporte a items sem o Present Box pelo Item Shop. * Adicionado os items de brinde (Presente de Natal).
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2912 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- lib/darghos/lib_actions.lua
+++ lib/darghos/lib_actions.lua
@@ -6,6 +6,13 @@ function defaultActions(cid, item, fromPosition, itemEx, toPosition)
 	
 	local ret = false
 	
+	local CHRISTMAS_PRESENTS = {
+		CUSTOM_ITEMS.GREEN_CHRISTMAS_PRESENT,
+		CUSTOM_ITEMS.RED_CHRISTMAS_PRESENT,
+		CUSTOM_ITEMS.BLUE_CHRISTMAS_PRESENT,
+		CUSTOM_ITEMS.BIG_CHRISTMAS_PRESENT,
+	}
+	
 	if(item_id == CUSTOM_ITEMS.TELEPORT_RUNE) then
 		ret = teleportRune.onUse(cid, item, fromPosition, itemEx, toPosition)
 	elseif(item_id == CUSTOM_ITEMS.UNHOLY_SWORD) then
@@ -14,9 +21,150 @@ function defaultActions(cid, item, fromPosition, itemEx, toPosition)
 		ret = premiumScroll.onUse(cid, item, fromPosition, itemEx, toPosition)
 	elseif(item_id == CUSTOM_ITEMS.OUTFIT_TICKET) then
 		ret = outfitTicket.onUse(cid, item, fromPosition, itemEx, toPosition)
+	elseif(isInArray(CHRISTMAS_PRESENTS, item_id)) then
+		ret = christmasPresent.onUse(cid, item, fromPosition, itemEx, toPosition)
+	end
+
+	return ret
+end
+
+christmasPresent = {}
+
+function christmasPresent.onUse(cid, item, fromPosition, itemEx, toPosition)
+
+	local items = {
+		purple_knife = {id = 10513},
+		red_knife = {id = 10511},
+		blue_knife = {id = 10515},
+		brotherhood_ticket = {id = 12691, actionid = 19},
+		nightmare_ticket = {id = 12691, actionid = 17},
+		wizard_ticket = {id = 12691, actionid = 10},
+		pirate_ticket = {id = 12691, actionid = 12},
+		oriental_ticket = {id = 12691, actionid = 11},
+		assassin_ticket = {id = 12691, actionid = 13},
+		barbarian_ticket = {id = 12691, actionid = 8},
+		hunter_ticket = {id = 12691, actionid = 2},
+		warrior_ticket = {id = 12691, actionid = 7},
+		mage_ticket = {id = 12691, actionid = 7},
+		summoner_ticket = {id = 12691, actionid = 6},
+		warmaster_ticket = {id = 12691, actionid = 22},
+		yalaharian_ticket = {id = 12691, actionid = 21},
+		wayfarer_ticket = {id = 12691, actionid = 23},
+		soft_boots = {id = 6132},
+		firewalker_boots = {id = 9933},
+		nightmare_shield = {id = 6391},
+		necromancer_shield = {id = 6433},
+		spellbook_of_dark_mysteries = {id = 8918},
+		royal_crossbow = {id = 8851},
+		zaoan_helmet = {id = 11302},
+		zaoan_legs = {id = 11304},
+		fireborn_giant_armor = {id = 8881},
+		master_archer_armor = {id = 8888},
+		hellforged_axe = {id = 8924},
+		obsidian_truncheon = {id = 8928},
+		emerald_sword = {id = 8930},
+		windborn_colossus_armor = {id = 8883},
+		yalahari_armor = {id = 9776},
+		guardian_boots = {id = 11240},
+		yalahari_mask = {id = 9778},
+		the_devileye = {id = 8852},
+		great_shield = {id = 2522},
+		golden_boots = {id = 2646},
+		demon_legs = {id = 2495},
+		earthborn_titan_armor = {id = 8882},
+		royal_draken_mail = {id = 12642},
+		yalahari_leg_piece = {id = 9777},
+		elite_draken_helmet = {id = 12645}
+	}
+
+	local SORTED_ITEMS = {
+		[CUSTOM_ITEMS.GREEN_CHRISTMAS_PRESENT] = {
+			knife = { items.purple_knife, items.red_knife, items.blue_knife }, 
+			outfit = { items.brotherhood_ticket, items.nightmare_ticket, items.wizard_ticket, pirate_ticket, oriental_ticket },
+			item1 = { items.soft_boots, items.firewalker_boots },
+			item2 = { items.nightmare_shield, items.necromancer_shield }
+		},
+		[CUSTOM_ITEMS.RED_CHRISTMAS_PRESENT] = {
+			knife = { items.purple_knife, items.red_knife, items.blue_knife }, 
+			outfit = { items.assassin_ticket, items.barbarian_ticket, items.hunter_ticket, items.warrior_ticket },
+			item1 = { items.spellbook_of_dark_mysteries, items.royal_crossbow },
+			item2 = { items.zaoan_helmet, items.zoan_legs }
+		},
+		[CUSTOM_ITEMS.BLUE_CHRISTMAS_PRESENT] = {
+			knife = { items.purple_knife, items.red_knife, items.blue_knife }, 
+			outfit = { items.mage_ticket, items.summoner_ticket, items.warmaster_ticket },
+			item1 = { items.fireborn_giant_armor, items.master_archer_armor, items.hellforged_axe, items.obsidian_truncheon, items.emerald_sword },
+			item2 ={ items.windborn_colossus_armor, items.yalahari_armor, items.guardian_boots, items.yalahari_mask }
+		},
+		[CUSTOM_ITEMS.BIG_CHRISTMAS_PRESENT] = {
+			knife = { items.purple_knife, items.red_knife, items.blue_knife }, 
+			outfit = { items.yalaharian_ticket, items.wayfarer_ticket },
+			item1 = { items.the_devileye, items.great_shield, items.golden_boots, items.demon_legs },
+			item2 = { items.earthborn_titan_armor, items.royal_draken_mail, items.yalahari_leg_piece, items.elite_draken_helmet }
+		}
+	}
+	
+	local date = os.date("*t")
+	
+	if(item.actionid == aid.OPEN_CHRISTMAS_PRESENT) then
+	
+		-- Somente pode abrir depois da meia noite :P
+		if(date.day < 25 and date.month == 12 and date.year == 2011) then			
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Hey! Ainda não é dia de natal! Tenha paciencia e aguarde até a noite de natal!")
+			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
+			return true
+		end
+		
+		-- Ok, abrindo o presente...
+		local presentContent = SORTED_ITEMS[item.itemid]	
+		local present = doCreateItemEx(item.itemid)
+		
+		local sortedOutfit = math.random(1, #presentContent.outfit)		
+		local toCreateItems = {
+			presentContent.knife[math.random(1, #presentContent.knife)].id,
+			presentContent.outfit[sortedOutfit].id,
+			presentContent.item1[math.random(1, #presentContent.item1)].id,
+			presentContent.item2[math.random(1, #presentContent.item2)].id
+		}		
+
+		local success = true
+		local addedItems = {}
+		
+		for k,v in pairs(toCreateItems) do
+			local _item = doAddContainerItem(item.uid, v)
+			if (not _item) then
+				success = false
+				error("Não foi possivel adicionar um item ao presente de natal do jogador " .. getPlayerName(cid) .. ". Uid: ".. _item .. "")
+				break
+			else
+				if( k == 2) then
+					doItemSetActionId(_item, presentContent.outfit[sortedOutfit].actionid)
+				end
+				table.insert(addedItems, _item)
+			end
+		end		
+		
+		if(not success) then
+			for k,v in pairs(addedItems) do
+				doRemoveItem(_item)
+			end
+			
+			doPlayerSendCancel(cid, "Não foi possivel entregar o seu presente! Cerfique de possuir espaço e capacidade sulficiente.")
+			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
+		end
+
+		
+		if(success) then
+			doItemSetActionId(item.uid, 0)
+			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_FIREWORK_RED)
+			doCreatureSay(cid, "HoHoHo Feliz Natal!", TALKTYPE_MONSTER)
+			return false
+		end
+		
+		return true
 	end
 	
-	return ret
+	return false
 end
 
 unholySword = {}
@@ -157,17 +305,17 @@ function teleportRune.onUse(cid, item, frompos, item2, topos)
 	local onIsland = (getPlayerStorageValue(cid, sid.IS_ON_TRAINING_ISLAND) == 1) and true or false
 	
 	if(onIsland or teleportScrollIsLocked(cid)) then
-		doPlayerSendCancel(cid, "VocÃª nÃ£o pode usar este item neste lugar!")
+		doPlayerSendCancel(cid, "Você não pode usar este item neste lugar!")
 		return true
 	end
 
 	if(hasCondition(cid, CONDITION_INFIGHT) == TRUE) then
-		doPlayerSendCancel(cid, "VocÃª nÃ£o pode usar este item enquanto estiver em batalha!")
+		doPlayerSendCancel(cid, "Você não pode usar este item enquanto estiver em batalha!")
 		return true
 	end
 	
 	if(getPlayerStorageValue(cid, sid.TELEPORT_RUNE_STATE) ~= teleportRune.STATE_NONE) then
-		doPlayerSendCancel(cid, "A carga jÃ¡ sendo carregada, tenha paciencia!")
+		doPlayerSendCancel(cid, "A carga já sendo carregada, tenha paciencia!")
 		return true
 	end
 	
@@ -176,12 +324,12 @@ function teleportRune.onUse(cid, item, frompos, item2, topos)
 		local secondsLeft = (lastTeleportRuneUsage + teleportRune.TELEPORT_USAGE_INTERVAL) - os.time()
 		
 		if(secondsLeft >= 60) then
-			doPlayerSendCancel(cid, "VocÃª deve aguardar " .. math.floor(secondsLeft / 60) .. " minutos para que sua teleport rune descanÃ§e e possa usar-la novamente.")
+			doPlayerSendCancel(cid, "Você deve aguardar " .. math.floor(secondsLeft / 60) .. " minutos para que sua teleport rune descançe e possa usar-la novamente.")
 		else
-			doPlayerSendCancel(cid, "VocÃª deve aguardar menos de um minuto para que sua teleport rune termine de descanÃ§ar e possa usar-la novamente.")
+			doPlayerSendCancel(cid, "Você deve aguardar menos de um minuto para que sua teleport rune termine de descançar e possa usar-la novamente.")
 		end
 		
-		doPlayerSendCancel(cid, "VocÃª deve aguardar " .. math.ceil(((lastTeleportRuneUsage + teleportRune.TELEPORT_USAGE_INTERVAL) - os.time()) / 60) .. " minutos para que sua teleport rune descanÃ§e e possa usar-la novamente.")
+		doPlayerSendCancel(cid, "Você deve aguardar " .. math.ceil(((lastTeleportRuneUsage + teleportRune.TELEPORT_USAGE_INTERVAL) - os.time()) / 60) .. " minutos para que sua teleport rune descançe e possa usar-la novamente.")
 		return true
 	end
 	
@@ -205,7 +353,7 @@ function teleportRune.firstStep(cid)
 	
 	if(teleportScrollIsLocked(cid)) then
 		setPlayerStorageValue(cid, sid.TELEPORT_RUNE_STATE, teleportRune.STATE_NONE)
-		doCreatureSay(cid, "Arggh! A teleport rune nÃ£o funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
+		doCreatureSay(cid, "Arggh! A teleport rune não funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
 		return	
 	end
 	
@@ -227,7 +375,7 @@ function teleportRune.secondStep(cid)
 	
 	if(teleportScrollIsLocked(cid)) then
 		setPlayerStorageValue(cid, sid.TELEPORT_RUNE_STATE, teleportRune.STATE_NONE)
-		doCreatureSay(cid, "Arggh! A teleport rune nÃ£o funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
+		doCreatureSay(cid, "Arggh! A teleport rune não funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
 		return	
 	end	
 	
@@ -249,7 +397,7 @@ function teleportRune.thirdStep(cid)
 	
 	if(teleportScrollIsLocked(cid)) then
 		setPlayerStorageValue(cid, sid.TELEPORT_RUNE_STATE, teleportRune.STATE_NONE)
-		doCreatureSay(cid, "Arggh! A teleport rune nÃ£o funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
+		doCreatureSay(cid, "Arggh! A teleport rune não funciona neste lugar! O transporte foi abortado!", TALKTYPE_ORANGE_1)
 		return	
 	end	
 	
