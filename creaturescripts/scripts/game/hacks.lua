476331439f68f77a9922ee5a7597559fb82db6a6
Leandro Perrotta <leandro_perrotta@hotmail.com>
2012-02-17 10:07:55 +0000

    * Fix map * Automana cast
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/trunk@3202 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/hacks.lua
+++ creaturescripts/scripts/game/hacks.lua
@@ -8,41 +8,43 @@ end
 
 local function checkCastMana(cid)
 
-	local hackstate = getPlayerStorageValue(cid, sid.HACKS_CASTMANA)
+	local tile = getTileInfo(getCreaturePosition(cid))
+	if(not tile.optional) then		
+		return
+	end
 	
-	if(hackstate == 1) then
-	
+	local attacked = getCreatureTarget(cid)
+	if(getBooleanFromString(attacked) and isInArray({"Marksman Target", "Hitdoll"}, getCreatureName(attacked))) then
 		local mana = getCreatureMana(cid)
 		local manamax = getCreatureMaxMana(cid)
 		local manachange = math.ceil(manamax / 2) -- default is half (50%)
 		local manalimit = manamax - math.ceil(manamax / 4) -- 75%
 
 		if(mana >= manalimit) then
-			doPlayerAddMana(cid, -(manachange))
+			doPlayerAddMana(cid, -(manachange), false)
 			doPlayerAddManaSpent(cid, manachange)
+			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_MAGIC_BLUE)
+			doCreatureSay(cid, "Automana...", TALKTYPE_MONSTER)
 		end
-	end
+	end	
 end
 
-local function checkPlayerBot(cid)
+danceEvents = {}
+
+function checkPlayerBot(cid)
 	
 	if(getPlayerGroupId(cid) ~= GROUPS_PLAYER_BOT) then
 		return
 	end
-	
-	local spells = {
-		{ vocationCheck = isSorcerer, words = "exura vita", manarequired = 160 }
-		,{ vocationCheck = isDruid, words = "exura vita", manarequired = 160 }
-		,{ vocationCheck = isPaladin, words = "exura vita", manarequired = 160 }
-		,{ vocationCheck = isKnight, words = "exana mort", manarequired = 65 }
-	}
-	
-	for k,v in pairs(spells) do
-		if(v.vocationCheck(cid) and getCreatureMana(cid) >= v.manarequired) then
-			doCreatureSay(cid, v.words)
-			return
-		end
+			
+	if(not danceEvents[cid]) then
+		danceEvents[cid] = addEvent(autoDance, 1000 * 10, cid)
 	end
+end
+
+function autoDance(cid)
+	doCreatureSetLookDirection(cid, math.random(NORTH, WEST))
+	danceEvents[cid] = nil
 end
 
 function onThink(cid, interval)
